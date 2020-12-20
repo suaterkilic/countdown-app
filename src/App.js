ec4d2614c3593438be48d33790607e582eeadb0c
suaterkilic <76135400+suaterkilic@users.noreply.github.com>
2020-12-20 22:59:45 +0300

    background image removed

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import CountDown from './components/CountDown';
-import ClockImage from './images/clock.jpg';
+
 const useStyles = makeStyles(theme => ({
   container: {
       display: 'flex',
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'center',
-      height: '100vh',
-      backgroundImage: `url(${ClockImage})`,
-      backgroundSize: 'cover',
-      backgroundRepeat: false
+      height: '100vh'
   }
 }))
 
